54341e6d9171392a691aa05f87d2c5a1fd296cac
Anastasia Uskova <uskova0aa@gmail.com>
2022-08-25 15:56:17 +0500

    completed first task homework №2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,18 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
+  let min = arr[0], max = arr[0], sum = 0, avg, counter = 0;
+ 
+  for (let i = 0; i < arr.length; i++) {
+    counter++;
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  avg = parseFloat((sum / counter).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
