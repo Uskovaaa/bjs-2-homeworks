29da163984a20f056ac7404b820ee3445b3ddf58
Anastasia Uskova <uskova0aa@gmail.com>
2022-08-29 14:24:56 +0500

    completed all tasks homework №3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
-  return result; // boolean
+  return result = (arr1.length === arr2.length) && (arr1.every((item, index, array) => item === arr2[index]));
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  
+  return resultArr = arr.filter(element => (element > 0) && (element % 3 === 0)).map(i => i * 10);
 }
