5508a5cb4d0a5475430b34234116712febe1466f
Anastasia Uskova <uskova0aa@gmail.com>
2022-08-30 09:51:38 +0500

    new release

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  return result = (arr1.length === arr2.length) && (arr1.every((item, index, array) => item === arr2[index]));
+  return (arr1.length === arr2.length) && (arr1.every((item, index, array) => item === arr2[index]));
 }
 
 function advancedFilter(arr) {
-  let resultArr;
   
-  return resultArr = arr.filter(element => (element > 0) && (element % 3 === 0)).map(i => i * 10);
+  return arr.filter(element => (element > 0) && (element % 3 === 0)).map(i => i * 10);
 }
