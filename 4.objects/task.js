32d5800f4da99cf67db3e8ef6d7cb7db519e6161
Anastasia Uskova <uskova0aa@gmail.com>
2022-09-05 23:37:14 +0500

    completed task homework №4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+let studentIvan = new Student('Ivan', 'male', 20);
+let studentOlga = new Student('Olga', 'female', 22);
+let studentElena = new Student('Elena', 'female', 38);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+}
+
+Student.prototype.getAverage = function () {
+  return this.average = (this.marks.reduce((acc, item) => acc += item, 0)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
+};
