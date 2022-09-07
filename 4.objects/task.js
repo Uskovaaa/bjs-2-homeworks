628f0ead2118f5d7203428eca9914aae80531103
Anastasia Uskova <uskova0aa@gmail.com>
2022-09-07 11:27:19 +0500

    corrected method addMarks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -21,7 +21,11 @@ Student.prototype.addMark = function (mark) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-  this.marks = marks;
+  if(this.marks === undefined){ 
+    this.marks = [marks];
+    } else {
+      this.marks.push(marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
