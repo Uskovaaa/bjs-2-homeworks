7ec09b486c914b2f5a9b3812f641d5a3535c487a
Anastasia Uskova <uskova0aa@gmail.com>
2022-11-07 15:38:00 +0500

    corrected methods findBookBy and giveBookByName

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -73,23 +73,14 @@ class Library {
   }
 
   findBookBy(type, value) {
-    for(let i = 0; i < this.books.length; i++) {
-      if(this.books[i][type] === value) {
-        return this.books[i];
-      }
-    }
-    return null;
+    const findResult = this.books.find((item) => item[type] === value);
+    return findResult || null;
   }
 
   giveBookByName(bookName) {
-    for(let i = 0; i < this.books.length; i++) {
-      if(this.books[i].name === bookName) {
-        let book = this.books[i];
-        this.books.splice(i, 1);
-        return book;
-      }
-    }
-    return null;
+    const findResult = this.books.find((item) => item.name === bookName);
+    this.books = this.books.filter((item) => item.name !== bookName);
+    return findResult || null;
   }
 }
 
