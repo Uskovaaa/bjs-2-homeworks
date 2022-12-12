d6add9ce419816a2b2525f4bac629027aaad8fc5
Anastasia Uskova <uskova0aa@gmail.com>
2022-12-12 09:33:30 +0500

    completed task homework №7

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,43 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = []; 
+
+  function wrapper(...args) {
+    const hash = args.join(','); // получаем правильный хэш
+    let objectInCache = cache.find((item) => item.hash == hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (objectInCache) { // если элемент не найден
+        console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+        return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({hash, value: result}) ; // добавляем элемент с правильной структурой
+    if (cache.length > 5) { 
+      cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый) 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  function wrapper(...args) {
+    wrapper.allCount++;  
+    if (!timeoutId) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+  }  
+  
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
